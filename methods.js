5b72d0d65598df1440ca4ec3db3d717ba77addc5
sumit <sumitmessi018@gmail.com>
2023-10-11 15:35:21 +0530

    learningfast

---
+++ methods.js
@@ -0,0 +1,26 @@
+let name = "sumit messi\""
+console.log(name.length)
+
+console.log(name.toLocaleUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("sumi","papa"))
+
+
+
+
+let name = "abhi"
+let friend = "vansh"
+console.log(name.concat("is a friend of " , friend , "okk"))
+
+let friend2 = "         maheswari     "
+console.log(friend2)
+console.log(friend2.trim())
+
+let fr = "sumit" + "abhishek" + "amit"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr)
